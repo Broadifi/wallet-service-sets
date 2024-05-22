c6887c0bb650fccbcb26ed254f065466d4fa5032
animeshd9 <animeshd838@gmail.com>
2024-05-22 18:16:31 +0530

    chore

--- src/controllers/wallets.js
+++ src/controllers/wallets.js
@@ -35,7 +35,7 @@ class WalletController {
       if(type === 'checkout.session.completed' && eventObj.payment_status === 'paid' && eventObj.status === 'complete'){
         const isWalletExist = await Wallet.findOne( { createdBy: eventObj.client_reference_id  } )
         if( isWalletExist ) {
-          const credit = Number(isWalletExist.credit) + Number(eventObj.amount_total)
+          const credit = Number(isWalletExist.credit) + eventObj.amount_total
           console.log(credit)
           const r = await Wallet.updateOne({createdBy: eventObj.client_reference_id}, {credit})
           return res.send({ data: r });
