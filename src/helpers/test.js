63a3f1137552a22dae1d4ca00391c09b60f78d4b
Anirban Das <anirban.btkol@gmail.com>
2024-10-16 02:08:59 +0530

    .

---
+++ src/helpers/test.js
@@ -0,0 +1,10 @@
+const { publisher, subscriber } = require('./pub-sub');
+
+publisher.emit('startBilling', {
+    id: '666049ee9354447f4740fa5f', 
+    type: 'instance', 
+    name: 'test', 
+    deployedOn: '666049ee9354447f4740fa5b', 
+    createdBy: '655c6921d80902db98b84385', 
+    createdAt: '2024-10-15T19:42:04.257+00:00'
+})
