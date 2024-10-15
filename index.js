c5d7b624d0f7987328dc1b3600bd970fbfe2d90d
Anirban Das <anirban.btkol@gmail.com>
2024-10-16 00:59:27 +0530

    feat: bill processer

--- index.js
+++ index.js
@@ -4,14 +4,9 @@
  */
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const { Database } = require('./config');
 const { App } = require('./src/App');
-const { agenda, jobDefinitions, defineHourlyBillingJob } = require('./src/helpers/agenda');
-
-// Define the job schema (matching Agenda's job document structure)
-const agendaJobSchema = new mongoose.Schema({}, { collection: 'agendaJobs', strict: false });
-const AgendaJobs = mongoose.model('agendaJobs', agendaJobSchema);
+require('./src/helpers/billing-processor');
 
 const initApp = async () => {
   try {
@@ -23,19 +18,7 @@ const initApp = async () => {
       express,
     });
     app.init();
-    
-    await agenda.start();
-    const jobs = await AgendaJobs.find();
-    jobs.forEach(job => {
-      const jobName = job.name;
-      if (!jobDefinitions.has(jobName)) {
-        console.log(`Redefining existing job: ${jobName}`);
-        defineHourlyBillingJob(jobName)
-        jobDefinitions.add(jobName);
-      }
-    });
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.log(`Error: ${error}`);
     process.exit(1);
   }
--- src/helpers/billing-processor.js
+++ src/helpers/billing-processor.js
@@ -1,68 +1,163 @@
-const { publisher, subscriber } = require('./pub-sub');
+const Agenda = require('agenda');
+const moment = require('moment');
+const mongoose = require('mongoose');
+const { float } = require('.');
 const { Billing } = require('../models/billing');
 const { instancesInfo } = require('../models/instance');
 const { Wallet } = require('../models/wallet');
-const { defineHourlyBillingJob, jobDefinitions } = require('./agenda');
+const { publisher, subscriber } = require('./pub-sub');
 
-// To be used for all the billing in the entire project
-subscriber.on('startBilling', async ( billingInfo, ack ) => {
-    try {
-        const { id, type, name, deployedOn, createdBy, createdAt } = billingInfo; 
-        
-        const [ instanceInfo, wallet ] = await Promise.all([
-            instancesInfo.findById( deployedOn ),
-            Wallet.findOne({ owner: createdBy })
-        ])
-        const { hourlyRate } = instanceInfo;
-        
-        // Check if the wallet has enough credit
-        if( !wallet || (float(wallet.credit) < float(hourlyRate))) {
-            throw new Error('Payment required');
-        }
 
-        const billing = new Billing({ 
-                userId: createdBy, 
-                deployedOn,
-                hourlyRate, 
-                startTime: createdAt,
-                usedBy: { 
-                    id, 
-                    type, 
-                    name
+
+
+class BillingProcessor {
+
+    constructor() {
+        this.collection = 'billingJobs';
+        this.agendaJobSchema = new mongoose.Schema({}, { collection: this.collection, strict: false });
+        this.agendaJobsModel = mongoose.model(this.collection, this.agendaJobSchema);
+        this.agenda = new Agenda({ 
+            db: { 
+                    address: process.env.MONGODB_URI,
+                    collection: this.collection,
+                    options: { 
+                        useNewUrlParser: true, 
+                        useUnifiedTopology: true 
+                    } 
                 } 
-            });
-        await billing.save();
-
-        const billingId = String(billing._id); 
-
-        // agenda
-        await defineHourlyBillingJob(billingId);
-        await agenda.every('1 hour', billingId, { billingId }); 
-        await ack();
-    } catch (error) {
-        console.log(error);
+        });
+        this.agendaJobs = new Set();
+        subscriber.on('startBilling', this.startBilling);
+        subscriber.on('stopBilling', this.stopBilling);
+        this.initAgenda();
     }
-})
 
-subscriber.on('stopBilling', async ( billingObj, ack ) => {
-    try {
-        const { id } = billingObj;
+    async initAgenda() {
+        await this.agenda.start();
+        const existingJobs = await this.agendaJobsModel.find();
+        existingJobs.forEach( async ({ name: job }) => {
+          if (!this.agendaJobs.has(job)) {
+            console.log(`Redefining existing job: ${job}`);
+            await this.defineHourlyBillingJob(job)
+            this.agendaJobs.add(job);
+          }
+        });
+    }
 
-        const billingId = await Billing.findOne({ 'usedBy.id': id }, { _id: 1 });
-
-        const jobs = await agenda.jobs({ "data.billingId": billingId });
-
-        if (!jobs[0]) {
+    async defineHourlyBillingJob ( agendaJobName ) {
+        this.agenda.define(agendaJobName, async (job) => {
+          try {
+            const { billingId } = job.attrs.data;
+            // get billing and wallet
+             const [billing, wallet] = await Promise.all([
+              Billing.findById(billingId),
+              Wallet.findOne({ owner: job.attrs.data.userId })
+            ])
+            if (!billing || !wallet) throw new Error('Bill or wallet not found');
+      
+            // if the wallet has enough credit
+            if (float(wallet.credit) >= float(billing.hourlyRate)) {
+              // update the wallet
+              wallet.credit = float(wallet.credit) - float(billing.hourlyRate);
+              wallet.totalSpent = float(wallet.totalSpent) + float(billing.hourlyRate);
+              // if it's a new month
+              const isNewMonth = !moment(job.attrs.lastRunAt).isSame(moment(), 'month');
+              if(isNewMonth) {
+                wallet.lastMonthSpent = wallet.currentMonthSpent
+                wallet.currentMonthSpent = '0';
+              }
+              wallet.currentMonthSpent = float(wallet.currentMonthSpent) + float(billing.hourlyRate);
+              // update the billing
+              billing.totalCost = float(billing.totalCost) + float(billing.hourlyRate);
+              billing.durationHours = moment.duration(moment().diff(moment(billing.startTime))).asHours()
+              // save to database
+              await Promise.all([ wallet.save(), billing.save()])
+              // add to local job definitions set
+              this.agendaJobs.add(agendaJobName);
+            } else {
+              // remove the job if the wallet does not have enough credit
+              const jobs = await agenda.jobs({ "data.billingId": billingId });
+              await jobs[0].remove();
+              await Billing.updateOne( { _id: jobs[0].attrs.data.billingId }, { isActive: false, endTime: moment().toISOString() })
+              this.agendaJobs.delete(agendaJobName);
+              // Handle insufficient credit 
+              console.log('Insufficient credit. Service will be stopped for user:', billing.userId);
+              publisher.emit('stopService', {
+                type: jobs[0].attrs.data.usedBy.type,
+                id: jobs[0].attrs.data.usedBy.id
+              })
+            }
+            console.log('Billing updated:', billingId);
+          } catch (error) {
+            console.error('Error updating billing hourly:', error);
+          }
+        });
+    }
+    
+    // To be used for all the billing in the entire project
+    async startBilling(billingInfo, ack) { 
+        try {
+            const { id, type, name, deployedOn, createdBy, createdAt } = billingInfo; 
+            
+            const [ instanceInfo, wallet ] = await Promise.all([
+                instancesInfo.findById( deployedOn ),
+                Wallet.findOne({ owner: createdBy })
+            ])
+            const { hourlyRate } = instanceInfo;
+            
+            // Check if the wallet has enough credit
+            if( !wallet || (float(wallet.credit) < float(hourlyRate))) {
+                throw new Error('Payment required');
+            }
+    
+            const billing = new Billing({ 
+                    userId: createdBy, 
+                    deployedOn,
+                    hourlyRate, 
+                    startTime: createdAt,
+                    usedBy: { 
+                        id, 
+                        type, 
+                        name
+                    } 
+                });
+            await billing.save();
+    
+            const billingId = String(billing._id); 
+    
+            // agenda
+            await this.defineHourlyBillingJob(billingId);
+            await this.agenda.every('1 hour', billingId, { billingId }); 
             await ack();
+        } catch (error) {
+            console.log(error);
         }
-        await jobs[0].remove();
-
-        await Billing.updateOne( { _id: billingId }, { isActive: false, endTime: moment().toISOString() } )
-        
-        jobDefinitions.delete(billingId);
-        
-        await ack();
-    } catch (error) {   
-        console.log(error);
     }
-})
+
+    async stopBilling(billingInfo, ack) {
+        try {
+            const { id } = billingInfo;
+    
+            const billingId = await Billing.findOne({ 'usedBy.id': id }, { _id: 1 });
+    
+            const jobs = await this.agenda.jobs({ "data.billingId": billingId });
+    
+            if (!jobs[0]) {
+                await ack();
+            }
+            await jobs[0].remove();
+    
+            await Billing.updateOne( { _id: billingId }, { isActive: false, endTime: moment().toISOString() } )
+            
+            this.agendaJobs.delete(billingId);
+            
+            await ack();
+        } catch (error) {   
+            console.log(error);
+        }
+    }
+    
+}
+
+new BillingProcessor();
+
