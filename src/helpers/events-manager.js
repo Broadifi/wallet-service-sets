cd954d3ee5b8c9c0c895c1f9c97bea120c639bf3
Anirban Das <anirban.btkol@gmail.com>
2024-10-24 00:51:15 +0530

    .

--- src/helpers/events-manager.js
+++ src/helpers/events-manager.js
@@ -2,11 +2,10 @@ const EventEmitter = require('events');
 const { publisher, subscriber } = require('./pub-sub');
 const { WalletController } = require('../controllers/wallets');
 const { paymentsController } = require('../controllers/payments');
-const { console } = require('inspector');
 
 // const internalEvents = new EventEmitter();
 
-paymentsController.events.on('payment:done', (userId, amount) => WalletController.updateWalletCredit(userId, amount));
+paymentsController.events.on('payment:done', async (userId, amount) => await WalletController.updateWalletCredit(userId, amount));
 
 WalletController.events.on('wallet:updated', (userId) => {
     console.log(`Wallet updated for user: ${userId}`)
